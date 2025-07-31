1cd9cfa78427b9a9e20826625ec7a21eadbf6564
varshithameda <163495267+varshithameda@users.noreply.github.com>
2025-07-31 14:53:17 +0530

    final commit

--- come.java
+++ come.java
@@ -1,5 +1,6 @@
 public class come {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("SCM polling to moinitor change commit in git hub");
     }
 }
